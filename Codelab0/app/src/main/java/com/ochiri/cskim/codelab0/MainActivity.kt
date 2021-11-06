cb527655c119b60800a4f8e1253ea7c51bebeb49
cskim <sprstone@naver.com>
2021-11-06 20:08:12 +0900

    Tweaking UI

---
+++ Codelab0/app/src/main/java/com/ochiri/cskim/codelab0/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.ochiri.cskim.codelab0
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ochiri.cskim.codelab0.ui.theme.Codelab0Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Codelab0Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Codelab0Theme {
+        Greeting("Android")
+    }
+}
