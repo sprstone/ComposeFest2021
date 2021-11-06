679beebe1761e55cd038a987da091388f2ef9bc3
cskim <sprstone@naver.com>
2021-11-06 22:47:11 +0900

    State hoisting

--- Codelab0/app/src/main/java/com/ochiri/cskim/codelab0/MainActivity.kt
+++ Codelab0/app/src/main/java/com/ochiri/cskim/codelab0/MainActivity.kt
@@ -3,17 +3,10 @@ package com.ochiri.cskim.codelab0
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +24,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
+fun MyApp()
+{
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if(shouldShowOnboarding) OnboardingScreen(onContinuclicked = {shouldShowOnboarding = false})
+    else Greetings()
+}
+
+@Composable
+fun OnboardingScreen(onContinuclicked: () -> Unit)
+{
+    Surface() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinuclicked) {
+                Text("Continue")
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
     Surface(color = MaterialTheme.colors.background) {
         Column(modifier = Modifier.padding(vertical = 4.dp)) {
             for (name in names) Greeting(name = name)
@@ -41,7 +63,11 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+
     val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -50,6 +76,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
