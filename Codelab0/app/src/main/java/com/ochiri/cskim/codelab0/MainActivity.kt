426ad29609cabe6043cda49ee8dba68b79522f40
cskim <sprstone@naver.com>
2021-11-07 11:43:16 +0900

    Styling and theming

--- Codelab0/app/src/main/java/com/ochiri/cskim/codelab0/MainActivity.kt
+++ Codelab0/app/src/main/java/com/ochiri/cskim/codelab0/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ochiri.cskim.codelab0
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ochiri.cskim.codelab0.ui.theme.Codelab0Theme
@@ -92,15 +94,23 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = "$name")
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             OutlinedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(if(expanded.value) "Show less" else "Show more")
             }
         }
     }
-
 }
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPeviewDark"
+)
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
