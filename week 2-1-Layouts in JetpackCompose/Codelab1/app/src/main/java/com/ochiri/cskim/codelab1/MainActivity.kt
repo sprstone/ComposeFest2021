8ccf4b0e851fe622e596ddbbaf5004b1208055c7
cskim <sprstone@naver.com>
2021-11-09 17:49:27 +0900

    Create custom layout

--- week 2-1-Layouts in JetpackCompose/Codelab1/app/src/main/java/com/ochiri/cskim/codelab1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Codelab1/app/src/main/java/com/ochiri/cskim/codelab1/MainActivity.kt
@@ -18,8 +18,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
@@ -44,6 +49,7 @@ class MainActivity : ComponentActivity() {
 
 
 
+@ExperimentalCoilApi
 @Composable
 fun LayoutsCodelab() {
     Scaffold(
@@ -60,36 +66,109 @@ fun LayoutsCodelab() {
             )
         }
     ) {
-            ScrollingList()
-            /*innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))*/
+            //innerPadding -> ScrollingList(Modifier.padding(innerPadding).padding(8.dp))
+            innerPadding ->
+        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    Codelab1Theme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    Codelab1Theme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
 @ExperimentalCoilApi
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier/*.padding(8.dp)*/) {
+    /*Column(modifier = modifier*//*.padding(8.dp)*//*) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
+    }*/
 
-        //Simplist()
-        //Lazylist()
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
     }
 }
 
 @ExperimentalCoilApi
 @Composable
-fun ScrollingList() {
+fun ScrollingList(modifier: Modifier = Modifier) {
+
     val listSize = 100
     val scrollState = rememberLazyListState()
     val coroutineScope = rememberCoroutineScope()
 
     Column {
-        Row(modifier = Modifier.padding(8.dp)) {
+        Row(modifier = modifier) {
             Button(onClick = {
                 coroutineScope.launch {
-                    // 0 is the first item index
                     scrollState.animateScrollToItem(0)
                 }
             }) {
@@ -99,7 +178,6 @@ fun ScrollingList() {
             Button(
                 onClick = {
                 coroutineScope.launch {
-                    // listSize - 1 is the last index of the list
                     scrollState.animateScrollToItem(listSize - 1)
                 }
             },
