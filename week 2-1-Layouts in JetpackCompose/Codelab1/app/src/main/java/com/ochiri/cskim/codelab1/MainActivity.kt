ca57c3a8c1bffba7fcfbf0c8042dd8f4d26bf196
cskim <sprstone@naver.com>
2021-11-09 15:47:58 +0900

    Working with lists

--- week 2-1-Layouts in JetpackCompose/Codelab1/app/src/main/java/com/ochiri/cskim/codelab1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Codelab1/app/src/main/java/com/ochiri/cskim/codelab1/MainActivity.kt
@@ -3,12 +3,10 @@ package com.ochiri.cskim.codelab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,13 +14,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.ochiri.cskim.codelab1.ui.theme.Codelab1Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +42,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun LayoutsCodelab() {
     Scaffold(
@@ -55,19 +59,103 @@ fun LayoutsCodelab() {
                 }
             )
         }
-    ) { innerPadding ->
-        BodyContent(
-            Modifier
-                .padding(innerPadding)
-                .padding(8.dp))
+    ) {
+            ScrollingList()
+            /*innerPadding ->
+        BodyContent(Modifier.padding(innerPadding).padding(8.dp))*/
     }
 }
 
+@ExperimentalCoilApi
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier/*.padding(8.dp)*/) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
+
+        //Simplist()
+        //Lazylist()
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(
+                onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            },
+            modifier = Modifier.padding(start = 8.dp)) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun Simplist(){
+
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100){
+            Text("Item #$it")
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun Lazylist(){
+    
+    val scrollState = rememberLazyListState()
+    
+    LazyColumn(state = scrollState){
+        items(100){
+            //Text("Item #$it")
+            ImageListItem(index = it)
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
